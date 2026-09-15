31533d9e1507d50e14f8f435cd8671337ddf9aa4
carlfremault <carlfremault@yahoo.com>
2026-09-15 11:41:54 +0200

    [Frontend] Base layout: title bar, side nav

--- app/ui/src/App.tsx
+++ app/ui/src/App.tsx
@@ -1,21 +1,5 @@
-import { Link, Navigate, Route, Routes } from "react-router";
-
-import Artists from "./pages/Artists";
-import Events from "./pages/Events";
+import AppRoutes from "./routes";
 
 export default function App() {
-  return (
-    <div className="sap-flex sap-flex--column sap-flex--gap-medium">
-      <nav className="sap-flex sap-flex--gap-small">
-        <Link to="/events">Events</Link>
-        <Link to="/artists">Artists</Link>
-      </nav>
-
-      <Routes>
-        <Route path="*" element={<Navigate to="/events" replace />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/artists" element={<Artists />} />
-      </Routes>
-    </div>
-  );
+  return <AppRoutes />;
 }
--- app/ui/src/main.tsx
+++ app/ui/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { setTheme } from "@ui5/webcomponents-base/dist/config/Theme.js";
 import { ThemeProvider } from "@ui5/webcomponents-react";
 
 import App from "./App";
@@ -8,6 +9,8 @@ import App from "./App";
 import "@ui5/webcomponents-react/dist/Assets.js";
 import "@fundamental-styles/common-css/dist/sap-flex.css";
 import "./index.css";
+
+setTheme("sap_horizon");
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
